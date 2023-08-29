44bdc4f5313e8b04f9f9f7131cddf752dbd4b812
Mahfuz Ahmed <mahfuzy555@gmail.com>
2023-08-29 13:00:13 +0600

    week 12 day 5

---
+++ week 12/day 5/C_1_Pokémon_Army_easy_version.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define read(type) readInt<type>() // Fast read
+#define ll long long
+#define nl "\n"
+
+void solve()
+{
+    int n, q;
+    cin >> n >> q;
+    ll arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int index = 0;
+    ll ans = 0;
+    ll currentlarge = arr[index];
+    index++;
+    while (index < n && arr[index] >= arr[index - 1])
+    {
+        currentlarge = arr[index];
+        index++;
+    }
+    ans += currentlarge;
+
+    while (index != n)
+    {
+        ll smaller = arr[index];
+        index++;
+        while (index < n && arr[index] <= arr[index - 1])
+        {
+            smaller = arr[index];
+            index++;
+        }
+        ll larger = 0;
+        while (index < n && arr[index] >= larger)
+        {
+            larger = arr[index];
+            index++;
+        }
+        if (larger - smaller >= 0)
+        {
+            ans += larger - smaller;
+        }
+        else
+        {
+            break;
+        }
+    }
+    cout << ans << nl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        solve();
+    }
+    return 0;
+}
+// check constraints
+// at first try a brute force way to solve
+// then optimization
+
+//  if its array:
+//  1. try greedy
+//  2. if it is a result of subproblems try dp
+//  3. if it is showing for  a range minimum or maximum
+//    3.1 tttttffffff
+//    else
+//    3.2 ffffftttttt
+//    try binary search
